5326dd78ad2668cbe1644f53efa760742c0b8520
vaibhav-soni-learnapp <125342282+vaibhav-soni-learnapp@users.noreply.github.com>
2023-10-27 15:05:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,13 @@
 import streamlit as st
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from io import StringIO
+import requests
 
 def download_html(url):
     response = requests.get(url)
     with open("downloaded_page.html", "w") as f:
         f.write(response.text)
-
-def extract_classes(file_path, classes):
-    with open(file_path, "r") as f:
-        soup = BeautifulSoup(f, 'html.parser')
-    
-    extracted_data_list = []
-    for class_name, class_list in classes.items():
-        elements = []
-        for single_class in class_list.split('.'):
-            if single_class:  # To skip empty strings
-                if '[' in single_class and ']' in single_class:
-                    # Special handling for class names with square brackets
-                    elements.extend([tag for tag in soup.find_all(class_=True) if single_class.replace('[', '\[').replace(']', '\]') in tag['class']])
-                else:
-                    elements.extend(soup.find_all(class_=single_class))
-        for element in elements:
-            extracted_data = {}
-            extracted_data[class_name] = element.text.strip()
-            extracted_data_list.append(extracted_data)
-    
-    return extracted_data_list
 
 # Streamlit UI
 st.title('HTML Scraper and Data Exporter')
@@ -40,17 +19,44 @@ download_button = st.button('Download HTML')
 if download_button:
     download_html(url)
 
-    # Define the classes and their labels
-    classes_to_extract = {
-        'mb_1_class_text': '.mb-1.overflow-hidden.break-words.font-sans.text-lg.font-[500].babybear:text-md',
-        'pr_05_pt_05_class_text': '.pr-0.5.pt-0.5',
-        'before_middot_pt_05_class_text': '.before:middot.pt-0.5',
-        'content_description_class_text': '.content-description.mt-0.5.break-words.font-sans.text-sm.font-normal.babybear:text-xs'
-    }
+    # Load the HTML file
+    with open("downloaded_page.html", 'r', encoding='utf-8') as file:
+        html_content = file.read()
 
-    extracted_data_list = extract_classes("downloaded_page.html", classes_to_extract)
-    # Convert list of dictionaries to DataFrame for easier CSV export
-    df = pd.DataFrame(extracted_data_list)
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Initialize empty lists to store the extracted text
+    mb_1_text = []
+    pr_05_pt_05_text = []
+    before_middot_pt_05_text = []
+    content_description_text = []
+
+    # Extract the text for each class and append to the respective list
+    for tag in soup.find_all(class_='mb-1 overflow-hidden break-words font-sans text-lg font-[500] babybear:text-md'):
+        mb_1_text.append(tag.text.strip())
+
+    for tag in soup.find_all(class_='pr-0.5 pt-0.5'):
+        pr_05_pt_05_text.append(tag.text.strip())
+
+    for tag in soup.find_all(class_='before:middot pt-0.5'):
+        before_middot_pt_05_text.append(tag.text.strip())
+
+    for tag in soup.find_all(class_='content-description mt-0.5 break-words font-sans text-sm font-normal babybear:text-xs'):
+        content_description_text.append(tag.text.strip())
+
+    # Create a DataFrame from the extracted text
+    df = pd.DataFrame({
+        'mb_1_class_text': mb_1_text,
+        'pr_05_pt_05_class_text': pr_05_pt_05_text,
+        'before_middot_pt_05_class_text': before_middot_pt_05_text,
+        'content_description_class_text': content_description_text
+    })
+
+    # Align the lengths of all lists to be the same for dataframe creation
+    max_len = max(len(mb_1_text), len(pr_05_pt_05_text), len(before_middot_pt_05_text), len(content_description_text))
+    for key, value in df.items():
+        df[key] = (value.tolist() + [None] * (max_len - len(value)))[:max_len]
 
     st.table(df)
 
